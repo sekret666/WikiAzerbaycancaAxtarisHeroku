c4604e9d32fbb2cd8fcf9b76b3234e92285c9a16
Mirza <hadiyev94@gmail.com>
2017-12-22 02:02:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,198 @@
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const recipepuppyHost = 'http://www.recipepuppy.com/api/?q=';
+const currencyConvertHost = "http://api.fixer.io/latest?";
+const chucknorrisHost = 'https://api.chucknorris.io/jokes/random';
+const wikiPediaApiHost = 'https://az.wikipedia.org/w/api.php?'; //https://www.mediawiki.org/wiki/API:Opensearch
+
+app.get('/dummyget', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'speech': 'dummy speech', 'displayText': 'dummy get works!' }));
+});
+
+
+app.post('/webhook', function (req, res) {
+
+    if (req.body.result.parameters['Bored']) {
+        callChuckNorrisFact()
+            .then((output) => {
+                let result = toApiAiResponseMessage(output.value, output.value, toTelgramObject(output.value, 'Markdown'));
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result));
+            })
+            .catch(errorHandler);
+    }
+    else if (req.body.result.parameters['FoodItem']) {
+        var fooditem = req.body.result.parameters['FoodItem'];
+        callRecipePuppy(fooditem)
+            .then((output) => {
+
+                let displayText = `Tapdığım məlumat: ${output.title} at ${output.href}`;
+                let telegramText = htmlEntities('*Found*-' + output.title + '\n' + '* Tərkibi aşağıdakılardır*-' + output.ingredients + '\n' + '* Siz onu yoxlaya bilərsiniz*- ' + output.href);
+                let result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(telegramText, 'Markdown'));
+                console.log(result);
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result));
+            })
+            .catch(errorHandler);
+    }
+    else if (req.body.result.parameters['currency-from'] && req.body.result.parameters['currency-to']) {
+        var currencyFrom = req.body.result.parameters['currency-from'];
+        var currencyTo = req.body.result.parameters['currency-to'];
+        var number = 1.0;
+        if (req.body.result.parameters['number']) {
+            number = parseFloat(req.body.result.parameters['number']);
+            if (number <= 0) {
+                number = 1.0;
+            }
+        }
+        callFixerIo(currencyFrom, currencyTo)
+            .then((output) => {
+                let resultText = Array();
+                currencyTo.forEach(function (cur) {
+                    var toNumber = number * parseFloat(output.rates[cur.toUpperCase()]);
+                    toNumber = toNumber.toFixed(3);
+                    resultText.push(`${number} ${output.base} = ${toNumber} ${cur}`);
+                }, this);
+
+                let displayText = resultText.join();
+                let result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(resultText.join('\n'), 'Markdown'));
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result));
+            });
+    }
+    else if (req.body.result.parameters['wikiaxtaristermini']) {
+        var searchTerm = req.body.result.parameters['wikiaxtaristermini'];
+        callWikiPediaApi(searchTerm)
+            .then((output) => {
+                let displayText = `${searchTerm} heçnə tapa bilmədim`;
+                let result;
+                if (output && output[0]) {
+                    displayText = `Vikipediyadan ${output[1][0]}: ${output[2][0]} haqqında tapdığım şeylər `;
+                    let telegramText = htmlEntities(`Vikipediyadan *${output[1][0]}*: ${output[2][0]} haqqında tapdığım məlumatlar \n\n Davamını oxu [WikiPedia](${output[3][0]})`);
+                    result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(telegramText, 'Markdown'));
+                }
+                res.setHeader('Content-Type', 'application/json');
+                if (result) {
+                    res.send(JSON.stringify(result));
+                }
+                else {
+                    res.send(JSON.stringify(displayText));
+                }
+            });
+    }
+    else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ 'speech': "Heçnə tapılmadı", 'displayText': "Heçnə tapılmadı" }));
+    }
+});
+
+
+function callRecipePuppy(fooditem) {
+    return new Promise((resolve, reject) => {
+        http.get(recipepuppyHost + fooditem, (res) => {
+            let body = '';
+            res.on('data', (d) => body += d);
+            res.on('end', () => {
+                let jO = JSON.parse(body);
+                let firstItem = jO.results[Math.floor((Math.random() * jO.results.length))];
+                resolve(firstItem);
+            });
+
+            res.on('error', (error) => {
+                reject(error);
+            });
+        });
+    });
+}
+
+function callFixerIo(currencyFrom, currencyTo) {
+    return new Promise((resolve, reject) => {
+        currencyTo = currencyTo.join().toUpperCase();
+        let url = `${currencyConvertHost}base=${currencyFrom}&symbols=${currencyTo}`;
+        http.get(url, (res) => {
+            let body = '';
+            res.on('data', (d) => body += d);
+            res.on('end', () => {
+                let jO = JSON.parse(body);
+                resolve(jO);
+            });
+
+            res.on('error', (error) => {
+                reject(error);
+            });
+        });
+    });
+}
+
+function callChuckNorrisFact() {
+    return new Promise((resolve, reject) => {
+        https.get(chucknorrisHost, (res) => {
+            let body = '';
+            res.on('data', (d) => body += d);
+            res.on('end', () => {
+                let jO = JSON.parse(body);
+                resolve(jO);
+            });
+
+            res.on('error', (error) => {
+                reject(error);
+            });
+        });
+    });
+}
+
+function callWikiPediaApi(searchTerm, format = "json", action = "opensearch", limit = 2, profile = "fuzzy") {
+    return new Promise((resolve, reject) => {
+        let url = `${wikiPediaApiHost}&format=${format}&action=${action}&limit=${limit}&profile=${profile}&search=${searchTerm}`;
+        https.get(url, (res) => {
+            let body = '';
+            res.on('data', (d) => body += d);
+            res.on('end', () => {
+                let jO = JSON.parse(body);
+                resolve(jO);
+            });
+            res.on('error', (error) => {
+                reject(error);
+            });
+        });
+    });
+}
+
+
+function toTelgramObject(text, parse_mode) {
+    return {
+        text: text,
+        parse_mode: parse_mode
+    }
+}
+
+function toApiAiResponseMessage(speech, displayText, telegramObject = null) {
+    return {
+        speech: speech,
+        displayText: displayText,
+        data: {
+            telegram: telegramObject
+        }
+    }
+}
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function errorHandler(error) {
+    console.log(error);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(toApiAiResponseMessage(error, error, toTelgramObject(error, 'Markdown'))));
+}
+
+app.listen((process.env.PORT || 5000), function () {
+    console.log("Server dinləyir");
+});
